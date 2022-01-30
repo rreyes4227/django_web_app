dc51c5ebe178bfda105e045543f36afa040b8eba
Ryan D Reyes <ryan.reyes2012@gmail.com>
2022-01-30 11:24:23 -0700

    product and order  model migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from sre_constants import CATEGORY
 from django.db import models
 
 
@@ -6,3 +7,32 @@ class Customer(models.Model):
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=20, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor')
+    )
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered')
+    )
+
+    # date_created =
+    # product =
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
