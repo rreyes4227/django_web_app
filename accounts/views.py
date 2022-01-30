1efc375f513d2fd0da503d0eadd8848a1fbd33fa
Ryan D Reyes <ryan.reyes2012@gmail.com>
2022-01-30 16:40:10 -0700

    populate products page

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 def home(request):
@@ -7,7 +8,8 @@ def home(request):
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
 
 def customer(request):
