10a5e5a3dfa8940c7d147375bd9a49ec92956dc4
Ryan D Reyes <ryan.reyes2012@gmail.com>
2022-01-30 18:05:23 -0700

    dynamic urls and button styling

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,18 @@ from .models import *
 
 
 def home(request):
-    return render(request, 'accounts/dboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders, 'customers': customers,
+               'total_customers': total_customers, 'total_orders': total_orders,
+               'delivered': delivered, 'pending': pending}
+
+    return render(request, 'accounts/dboard.html', context)
 
 
 def products(request):
@@ -12,5 +23,11 @@ def products(request):
     return render(request, 'accounts/products.html', {'products': products})
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+
+    context = {'customer': customer, 'orders': orders,
+               'order_count': order_count}
+    return render(request, 'accounts/customer.html', context)
