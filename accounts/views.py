c3e5a94aa69f1beb353123fd1afb6a5ad29104f0
Ryan D Reyes <ryan.reyes2012@gmail.com>
2022-02-02 14:24:08 -0700

    added delete and update functions

--- accounts/views.py
+++ accounts/views.py
@@ -42,5 +42,30 @@ def createOrder(request):
         if form.is_valid():
             form.save()
             return redirect('/')
+
     context = {'form': form}
     return render(request, 'accounts/order_form.html', context)
+
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
+
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
+
+    context = {'item': order}
+    return render(request, 'accounts/delete.html', context)
